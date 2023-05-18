01c8f6514d16cf8c5c5cc96ea3f1943d3ffd3197
xdangos_dev <abdallamfathy@gmail.com>
2023-05-18 10:06:26 +0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import ('tailwindcss').Config} */
+
+// eslint-disable-next-line no-undef
+module.exports = {
+  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'], // adjust this to match your file extensions (vue for Vue, tsx for React, etc.)
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
